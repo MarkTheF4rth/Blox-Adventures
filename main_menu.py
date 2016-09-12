b038601d270e18e6444dbbf98ebb81c0d697b86d
MII <mnel.jnr@gmail.com>
2016-09-12 08:01:22 +0100

    Replaced placeholder images with better ones, created a sound for mouse hover, implemented scaling into buttons

--- main_menu.py
+++ main_menu.py
@@ -4,12 +4,18 @@ import pygame
 
 class Button(object):
     def __init__(self, pos, size, label, images, status='normal'):
+        """initialises button object"""
         self.pos = pos
         self.size = size
         self.label = label
-        self.rect = pygame.Rect(pos[0],pos[1],size[0],size[1])
         self.images = images
         self.status = status
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+        self.colours = {
+            'normal': (58, 45, 233),
+            'active': (255, 255, 255), 
+            'inactive': (0, 0, 0)}
+        self.text_prep()
 
     def mouse_hover(self, mouse_pos):
         active = ['active', 'normal']
@@ -18,35 +24,64 @@ class Button(object):
                 self.status = 'active'
             else:
                 self.status = 'normal'
+
+    def text_prep(self):
+        """prepares the text that will be put on top of the button"""
+        self.text = {}
+
+        font = pygame.font.SysFont(None, min(self.size))
+        x_offset = ((self.rect[2]-font.size(self.label)[0])/2)
+        y_offset = (self.rect[3]/4)
+        normal_text = font.render(self.label, True, self.colours['normal'])
+        active_text = font.render(self.label, True, self.colours['active'])
+        inactive_text = font.render(self.label, True, self.colours['inactive'])
+        text_ref = {"normal":normal_text, "active":active_text,
+                "inactive":inactive_text}
+
+        self.text['pos'] = (x_offset+self.pos[0], y_offset+self.pos[1])
+        self.text['image'] = text_ref
     
     def draw(self, screen):
         status_ref = {'normal':0, 'active':1, 'inactive':2}
         image = self.images[status_ref[self.status]]
         image = pygame.transform.scale(image, self.size)
         screen.blit(image, self.pos)
+        screen.blit(self.text['image'][self.status], self.text['pos'])
         return screen
 
 class MainMenu(Button):
-    def __init__(self, first_play):
+    def __init__(self, first_play, screen):
         self.image_init()
-        self.button_init(first_play)
+        self.button_init(first_play, screen)
 
     def image_init(self):
         imagedir = 'Images'
-        normal = pygame.image.load(imagedir+"/menu_button_normal.png")
-        active = pygame.image.load(imagedir+"/menu_button_active.png")
-        inactive = pygame.image.load(imagedir+"/menu_button_inactive.png")
+        normal = pygame.image.load(imagedir+"/button_normal.png")
+        active = pygame.image.load(imagedir+"/button_active.png")
+        inactive = pygame.image.load(imagedir+"/button_inactive.png")
         self.images = (normal, active, inactive)
 
-    def button_init(self, first_play):
-        uniform_size = (1000, 200)
+    def button_init(self, first_play, screen):
+        """initialises all the buttons that will be displayed on the screen"""
+        screen_size = screen.get_size()
+        uniform_size_y = int(1/4 * screen_size[1])  # Find the size that y should be relative to the screen size
+        uniform_size_x = int(uniform_size_y * 4)               # Create x value from that, ration 4:1 x:y
+        uniform_size = (uniform_size_x, uniform_size_y)  # Put them together in a tuple
+
+        x_pos = int((screen_size[0] - uniform_size_x)/2)
+        y_space = int(((screen_size[1])-(3*uniform_size_y))/4)
+
+        pos = (x_pos, y_space)
+
         if first_play:
             game_label = "Begin"
         else:
             game_label = "Continue"
-        self.game = Button((10, 10), uniform_size, game_label, self.images)
-        self.options = Button((10, 210), uniform_size, 'options', self.images)
-        self.quit = Button((10,410), uniform_size, 'quit', self.images)
+        self.game = Button(pos, uniform_size, game_label, self.images)
+        pos = (x_pos, pos[1]+y_space+uniform_size_y)
+        self.options = Button(pos, uniform_size, 'Options', self.images)
+        pos = (x_pos, pos[1]+y_space+uniform_size_y)
+        self.quit = Button(pos, uniform_size, 'Quit', self.images)
         self.buttons = (self.game, self.options, self.quit)
 
     def run(self, events, screen):
@@ -58,7 +93,7 @@ class MainMenu(Button):
         if events.mouse_down[0]:
             for button in self.buttons:
                 if button.status == 'active':
-                    output = button.label
+                    output = button.label.lower()
 
         return screen, output
 
@@ -67,6 +102,7 @@ class Events(object):
         self.mouse_pos = mouse_pos
         self.current_event = None
 
+
 if __name__ == "__main__":
 
     WIDTH, HEIGHT = 1500, 900
@@ -74,9 +110,12 @@ if __name__ == "__main__":
     pygame.display.init()
     pygame.font.init()
     pygame.image.get_extended()
+    infoObject = pygame.display.Info()
 
+
+    FULLSCREEN = False
     SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-    MAINMENU = MainMenu(True)
+    MAINMENU = MainMenu(True, SCREEN)
     EVENTS = Events(pygame.mouse.get_pos())
 
     RUN = True
@@ -84,6 +123,13 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUN = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_f:
+                if not FULLSCREEN:
+                    SCREEN = pygame.display.set_mode((infoObject.current_w, infoObject.current_h), pygame.FULLSCREEN)
+                else:
+                    SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+                MAINMENU.button_init(True, SCREEN)
+                FULLSCREEN = not FULLSCREEN
             else:
                 EVENTS.current_event = event
 
